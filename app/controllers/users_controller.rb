5c2bd4290171e6ef40a186ff842ebdd66f76268f
pritam-das <pritam.das@mail.utoronto.ca>
2017-06-25 17:23:04 -0400

    basic user profile which can be edited

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,15 +19,32 @@ def create
   flash[:notice] = 'Signed up succesfully!'
   session[:user_id] = @user.id
   redirect_to root_url
-else
-  render :new
-end
+
+  else
+    render :new
+  end
 
 end
+
+def edit
+  @user = User.find(params[:id])
+end
+
+def update
+  @user = User.find(params[:id])
+
+  if @user.update_attributes(user_params)
+    flash[:notice] = 'User profile updated succesfully'
+    redirect_to user_path(@user)
+  end
+
+end
+
+
 
 private
 def user_params
-  params.require(:user).permit(:email, :password, :password_confirmation, :avatar, :bio)
+  params.require(:user).permit(:email, :password, :password_confirmation, :name, :avatar, :bio)
 end
 
 
