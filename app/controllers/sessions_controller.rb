9e8728952cc15e2e08e0847ab3df5f06b2d62125
pritam-das <pritam.das@mail.utoronto.ca>
2017-06-25 12:17:58 -0400

    Setup user sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,31 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user= User.find_by(email: params[:email])
+
+    #if the login details belong to a user
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      cookies.signed[:user_id] = user.id
+      flash[:notice] = "Logged in succesfully as #{user.email} !"
+      redirect_to root_url
+
+    else
+      flash[:alert] = 'Email and password combination does not match!'
+      render :new
+    end
+  end
+
+  def destroy
+    # if user is logged in, log out
+
+      session[:user_id] = nil
+      cookies.delete(:user_id)
+      flash[:notice] = 'User has been logged out succesfully!'
+      redirect_to events_url
+      #if host is logged in, log out
+
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,21 @@ def show
 end
 
 def create
+  @user = User.new(user_params)
 
+  if @user.save
+  flash[:notice] = 'Signed up succesfully!'
+  session[:user_id] = @user.id
+  redirect_to root_url
+else
+  render :new
+end
+
+end
+
+private
+def user_params
+  params.require(:user).permit(:email, :password, :password_confirmation, :avatar, :bio)
 end
 
 
