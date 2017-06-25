7ccecb467adcd09fe2cc11375cf0e90f0bac71b9
pritam-das <pritam.das@mail.utoronto.ca>
2017-06-25 13:02:10 -0400

    Set up basic user profile show page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def ensure_logged_in
+    unless current_user
+      flash[:alert] = "Please log in"
+      redirect_to new_session_url
+    end
+  end
+
+  def current_user
+    if session[:user_id]
+      return User.find(session[:user_id])
+    end
+  end
+
+  helper_method :current_user, :ensure_logged_in
 end
