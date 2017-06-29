b2b02db70e841c8558a009dea533db9048d224d1
pritam-das <pritam.das@mail.utoronto.ca>
2017-06-29 00:12:13 -0400

    Set up the associations between user, conversation, personal message

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,2 +1,8 @@
 class Conversation < ApplicationRecord
+
+  belongs_to :author, class_name: 'User'
+  belongs_to :receiver, class_name: 'User'
+
+  validates :author, uniqueness: {scope: :receiver}
+
 end
